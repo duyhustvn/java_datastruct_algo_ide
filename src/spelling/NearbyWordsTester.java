202dc3990d21c205817ff4780808009fd59be96f
duyleomessi <duyleitbka95@gmail.com>
2018-09-10 01:40:32 +0700

    implement suggestion but the executed time is slow

--- src/spelling/NearbyWordsTester.java
+++ src/spelling/NearbyWordsTester.java
@@ -20,12 +20,45 @@ public class NearbyWordsTester {
 
     }
 
+//    @Test
+//    public void testSubstituation() {
+//        String word = "hello";
+//        d.addWord(word);
+//        nearbyWords = new NearbyWords(d);
+//        nearbyWords.substitution("hellp", currentList, true);
+//        System.out.println(currentList);
+//        assertEquals("Test substituation ", "hello", currentList.get(0));
+//    }
+
+//    @Test
+//    public void testInsertion() {
+//        String word = "hello";
+//        d.addWord(word);
+//        d.addWord("shell");
+//        nearbyWords = new NearbyWords(d);
+//        nearbyWords.insertions("hell", currentList, true);
+//        System.out.println(currentList);
+//        assertEquals("Test insertion: ", "hello" , currentList.get(0));
+//    }
+
+//    @Test
+//    public void testDelete() {
+//        String word = "hello";
+//        d.addWord(word);
+//        nearbyWords = new NearbyWords(d);
+//        nearbyWords.deletions("hello", currentList, false);
+//        System.out.println(currentList);
+//        assertEquals("Test insertion: ", "hello" , currentList.get(0));
+//    }
+
     @Test
-    public void testSubstituation() {
-        String word = "hello";
-        d.addWord(word);
+    public void testSuggestion() {
+        String word = "hellp";
+        DictionaryLoader.loadDictionary(d, "data/dict.txt");
         nearbyWords = new NearbyWords(d);
-        nearbyWords.substitution("hellp", currentList, true);
-        assertEquals("Test substituation ", "hello", currentList.get(0));
+        long startTime = System.nanoTime();
+        nearbyWords.suggestions(word, 6);
+        long endTime = System.nanoTime();
+        System.out.println("it take " + (endTime - startTime) / 1000000000.0 + " seconds");
     }
 }
